f60eedf6a3a074a63ea3647b565901769d755856
Yann Dipita <dipitay@gmail.com>
2023-08-05 00:03:50 +0200

    cover image fixed

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     title: `${project.name} | Project`,
     description: project.tagline,
     openGraph: {
-      images: project.coverImage?.image || "project.png",
+      images: project.coverImage?.image || "ios-quiz-app.png",
       title: project.name,
       description: project.tagline,
     },
