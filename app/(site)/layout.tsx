4dd98b98a77d313ea8e7804b50745073eee397be
Yann Dipita <dipitay@gmail.com>
2023-08-17 01:01:04 +0200

    title changed

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yann DIPITA's Portfolio",
+  title: "Yann DIPITA",
   description: " this is Yann DIPITA portfolio",
   openGraph: {
     images: "https://letcreateanapp.com/wp-content/uploads/2022/03/Quiz-App-in-Swift.jpg",
