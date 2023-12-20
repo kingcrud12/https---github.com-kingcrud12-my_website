a0b9a7980b9f1b70b52984b1433453abcd070cfa
kingcrud12 <dipitay@gmail.com>
2023-12-21 00:22:12 +0100

    update background color

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,7 @@
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,14 +10,21 @@ export const metadata: Metadata = {
   title: "Yann DIPITA",
   description: " this is Yann DIPITA portfolio",
   openGraph: {
-    images: "https://letcreateanapp.com/wp-content/uploads/2022/03/Quiz-App-in-Swift.jpg",
+    images:
+      "https://letcreateanapp.com/wp-content/uploads/2022/03/Quiz-App-in-Swift.jpg",
   },
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>
+      <body
+        className={`${inter.className}bg-gradient-to-r from-[#e66465] to-[#9198e5] text-white`}
+      >
         <Navbar />
         {children}
         <Footer />
