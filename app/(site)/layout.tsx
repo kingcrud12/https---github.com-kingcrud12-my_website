77cc8388ef78a889c6c15cb522a5e28e3b3c6c8e
kingcrud12 <dipitay@gmail.com>
2023-12-24 00:02:23 +0100

    update language

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={`${inter.className} bg-zinc-900 text-white`}>
         <Navbar />
         {children}
