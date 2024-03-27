6bf7bc7a01be64ac6e760b1d4411115e9a02ac98
kingcrud12 <dipitay@gmail.com>
2024-03-27 18:36:42 +0100

    test

--- cypress/e2e/about.cy.ts
+++ cypress/e2e/about.cy.ts
@@ -5,7 +5,7 @@ describe('template spec', () => {
     it('about section link works fine', () => {
       cy.getByData("about").find("a").contains("A propos").click()
       cy.location("pathname").should("equal", "/about")
-      //cy.getByData("download").find("a").contains("Download Resumé").click()
+      cy.getByData("download").find("a").contains("Download Resumé").click()
     })
   })
   
